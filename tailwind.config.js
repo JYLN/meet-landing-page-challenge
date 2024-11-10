31e89f9c53746a4002aec27e5120057f7b0a1e1f
Jaylen Baxter <14062951+JYLN@users.noreply.github.com>
2024-11-10 15:57:29 -0700

    style: :lipstick: update global styles and tailwind config with custom font and colors
    - Install `@fontsource-variable/red-hat-display`
    - Import font in `styles.css`
    - Update tailwind config with color palette from design
    - Extend tailwind config with Red Hat Display font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./src/**/*.html"],
   theme: {
-    extend: {},
+    colors: {
+      cyan: {
+        300: "#8FE3F9",
+        600: "#4D96A9",
+      },
+      purple: {
+        300: "#D9B8FF",
+        600: "#855FB1",
+      },
+      slate: {
+        300: "#D1D1DF",
+        600: "#87879D",
+        900: "#28283D",
+      },
+      white: "#FAFAFA",
+    },
+    extend: {
+      fontFamily: {
+        redhat: ["Red Hat Display", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
