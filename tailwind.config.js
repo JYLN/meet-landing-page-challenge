230ecef7204f0298cc3f9d9e181e6c21a099f220
Jaylen Baxter <14062951+JYLN@users.noreply.github.com>
2024-11-10 20:47:41 -0700

    style: :lipstick: finalize project styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,12 @@ module.exports = {
       cyan: {
         300: "#8FE3F9",
         600: "#4D96A9",
+        hover: "#71C0D4",
       },
       purple: {
         300: "#D9B8FF",
         600: "#855FB1",
+        hover: "#B18BDD",
       },
       slate: {
         300: "#D1D1DF",
@@ -19,8 +21,16 @@ module.exports = {
       white: "#FAFAFA",
     },
     extend: {
+      backgroundImage: {
+        "footer-mobile":
+          "linear-gradient(to bottom, hsla(192,37%,48%,.90), hsla(192,37%,48%,.90)), url('./assets/mobile/image-footer.jpg')",
+        "footer-tablet":
+          "linear-gradient(to bottom, hsla(192,37%,48%,.90), hsla(192,37%,48%,.90)), url('./assets/tablet/image-footer.jpg')",
+        "footer-desktop":
+          "linear-gradient(to bottom, hsla(192,37%,48%,.90), hsla(192,37%,48%,.90)), url('./assets/desktop/image-footer.jpg')",
+      },
       fontFamily: {
-        redhat: ["Red Hat Display", "sans-serif"],
+        redhat: ["Red Hat Display Variable", "sans-serif"],
       },
     },
   },
